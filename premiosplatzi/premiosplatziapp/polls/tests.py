dcd33009b6e7c551a948e61de0b1368cffd6a7b1
Luciano Labudia <labudia.luciano@gmail.com>
2022-11-02 15:50:30 -0300

    Creando tests para DetailView

--- premiosplatzi/premiosplatziapp/polls/tests.py
+++ premiosplatzi/premiosplatziapp/polls/tests.py
@@ -97,3 +97,26 @@ class QuestionIndexViewTests(TestCase):
         create_question(question_text="Future question 2", days=40)
         response = self.client.get(reverse("polls:index"))        
         self.assertQuerysetEqual(response.context["latest_question_list"], []) 
+
+
+class QuestionDetailViewTests(TestCase):
+
+    def test_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future
+        returns a 404 error not found
+        """
+        future_question = create_question(question_text="Future question", days=30)
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past displays
+        the question's text
+        """
+        past_question = create_question(question_text="Past question", days=-30)
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response, past_question.question_text)
